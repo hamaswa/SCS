4f8ac7976804dff578a16a32958511d3f0aca1b5
Abdul Munem <munem.khetran@gmail.com>
2019-08-20 23:20:55 +0500

    changes css and color table structure to div

--- resources/views/admin/permissions/index.blade.php
+++ resources/views/admin/permissions/index.blade.php
@@ -17,138 +17,30 @@
         <div class="box-header"><h3>Permissions</h3></div>
         <div class="box-body">
             <div class="col-md-4 col-sm-12">
-                <div class="table-responsive">
-                    <table class="table table-bordered table-condensed table-hover table-striped">
-                        <thead class="bg-light-blue-gradient">
-                        <tr>
-                            <th class="col-lg-4 text-center">User Role</th>
-                            {{--<th class="col-lg-4 text-center">Assign</th>--}}
-                            <th class="col-lg-4 text-center">Users</th>
-                            <td>Action</td>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($roles as $role)
-                            <tr>
-                                <td colspan="3">
-                                    <form method="post" action="{{ route("permissions.update",$role->id) }}"
-                                          class="permission_form">
-                                        @method('patch')
-                                        @csrf()
-                                        <div class="col-lg-4">{{$role->name }}</div>
-                                        {{--<div class="col-lg-4"><input type="checkbox" checked/></div>--}}
-                                        <div class="col-lg-4">
-                                            <select multiple="multiple" name="users[]" class="users">
-                                                <?php
-                                                echo App\Http\Controllers\Admin\PermissionController::userOptions($users, $role)
-                                                ?>
-                                            </select>
-                                        </div>
-                                        <div class="col-lg-4"><input type="submit" value="Update"></div>
+                <div class="col-sm-4 bg-light-blue-gradient padding-5"><b> User Role </b></div>
+                <div class="col-sm-5 bg-light-blue-gradient padding-5"><b> Users </b></div>
+                <div class="col-sm-3 bg-light-blue-gradient padding-5"><b> Action </b></div>
+                <div class="clearfix"></div>
+                @foreach($roles as $role)
 
-                                    </form>
-                                </td>
+                    <form method="post" action="{{ route("permissions.update",$role->id) }}" class="permission_form">
+                        @method('patch')
+                        @csrf()
+                        <div class="col-sm-4 padding-5">{{$role->name }}</div>
+                        {{--<div class="col-lg-4"><input type="checkbox" checked/></div>--}}
+                        <div class="col-sm-5 padding-5">
+                            <select multiple="multiple" name="users[]" class="users">
+                                <?php
+                                echo App\Http\Controllers\Admin\PermissionController::userOptions($users, $role)
+                                ?>
+                            </select>
+                        </div>
+                        <div class="col-sm-3 padding-5"><input type="submit" value="Update" class="btn btn-primary btn-sm"></div>
 
-
-                            </tr>
-                        @endforeach
-                        <tr>
-                            <td colspan="3">
-                                <div class="col-lg-4"><input type="submit" id="update_all" value="Update All"></div>
-                            </td>
-                        </tr>
-                        {{--<tr>--}}
-                        {{--<td>Maker</td>--}}
-                        {{--<td class="text-center"><input type="checkbox" checked/></td>--}}
-                        {{--<td><select multiple="multiple" class="users">--}}
-                        {{--@foreach($users as $user)--}}
-                        {{--<option value="{{$user->id}}">{{$user->username}}</option>--}}
-
-                        {{--@endforeach--}}
-                        {{--</select></td>--}}
-                        {{--</tr>--}}
-                        {{--<tr>--}}
-                        {{--<td>Checker</td>--}}
-                        {{--<td class="text-center"><input type="checkbox" checked/></td>--}}
-                        {{--<td><select multiple="multiple" class="users">--}}
-                        {{--@foreach($users as $user)--}}
-                        {{--<option value="{{$user->id}}">{{$user->username}}</option>--}}
-
-                        {{--@endforeach--}}
-                        {{--</select></td>--}}
-                        {{--</tr>--}}
-                        {{--<tr>--}}
-                        {{--<td>Uploader</td>--}}
-                        {{--<td class="text-center"><input type="checkbox" checked/></td>--}}
-                        {{--<td><select multiple="multiple" class="users">--}}
-                        {{--@foreach($users as $user)--}}
-                        {{--<option value="{{$user->id}}">{{$user->username}}</option>--}}
-
-                        {{--@endforeach--}}
-                        {{--</select></td>--}}
-                        {{--</tr>--}}
-                        {{--<tr>--}}
-                        {{--<td>Requestor</td>--}}
-                        {{--<td class="text-center"><input type="checkbox" checked/></td>--}}
-                        {{--<td><select multiple="multiple" class="users">--}}
-                        {{--@foreach($users as $user)--}}
-                        {{--<option value="{{$user->id}}">{{$user->username}}</option>--}}
-
-                        {{--@endforeach--}}
-                        {{--</select></td>--}}
-                        {{--</tr>--}}
-                        {{--<tr>--}}
-                        {{--<td>Processor</td>--}}
-                        {{--<td class="text-center"><input type="checkbox" checked/></td>--}}
-                        {{--<td><select multiple="multiple" class="users">--}}
-                        {{--@foreach($users as $user)--}}
-                        {{--<option value="{{$user->id}}">{{$user->username}}</option>--}}
-
-                        {{--@endforeach--}}
-                        {{--</select></td>--}}
-                        {{--</tr>--}}
-                        {{--<tr>--}}
-                        {{--<td>Data Entry</td>--}}
-                        {{--<td class="text-center"><input type="checkbox" checked/></td>--}}
-                        {{--<td><select multiple="multiple" class="users">--}}
-                        {{--@foreach($users as $user)--}}
-                        {{--<option value="{{$user->id}}">{{$user->username}}</option>--}}
-
-                        {{--@endforeach--}}
-                        {{--</select></td>--}}
-                        {{--</tr>--}}
-                        {{--<tr>--}}
-                        {{--<td>Executor</td>--}}
-                        {{--<td class="text-center"><input type="checkbox"/></td>--}}
-                        {{--<td><select multiple="multiple" class="users">--}}
-                        {{--@foreach($users as $user)--}}
-                        {{--<option value="{{$user->id}}">{{$user->username}}</option>--}}
-
-                        {{--@endforeach--}}
-                        {{--</select></td>--}}
-                        {{--</tr>--}}
-                        {{--<tr>--}}
-                        {{--<td>Admin 1</td>--}}
-                        {{--<td class="text-center"><input type="checkbox"/></td>--}}
-                        {{--<td><select multiple="multiple" class="users">--}}
-                        {{--@foreach($users as $user)--}}
-                        {{--<option value="{{$user->id}}">{{$user->username}}</option>--}}
-
-                        {{--@endforeach--}}
-                        {{--</select></td>--}}
-                        {{--</tr>--}}
-                        {{--<tr>--}}
-                        {{--<td>Admin 2</td>--}}
-                        {{--<td class="text-center"><input type="checkbox"/></td>--}}
-                        {{--<td><select multiple="multiple" class="users">--}}
-                        {{--@foreach($users as $user)--}}
-                        {{--<option value="{{$user->id}}">{{$user->username}}</option>--}}
-
-                        {{--@endforeach--}}
-                        {{--</select></td>--}}
-                        {{--</tr>--}}
-                        </tbody>
-                    </table>
+                    </form>
+                @endforeach
+                <div class="col-sm-12 text-center">
+                    <input type="submit" class="btn btn-primary" id="update_all" value="Update All">
                 </div>
             </div>
             <div class="col-md-8 col-sm-12">
