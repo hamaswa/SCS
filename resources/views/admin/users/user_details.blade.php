847846bdd9372b8e73b7af6a741b4a0f74e9d7da
Hamayun Khan <hkhan.swa@gmail.com>
2019-08-21 10:08:12 +0500

    User Popup detials on permission page

--- resources/views/admin/users/user_details.blade.php
+++ resources/views/admin/users/user_details.blade.php
@@ -7,14 +7,18 @@
  */
 
 ?>
-<table>
+<table class="table table-responsive">
     <tr><th>Name</th><th>Email</th><th>User Name</th><th>Country</th></tr>
     <tr><td>{{$user->first_name}} {{ $user->last_name }}</td><td>{{$user->email}}</td>
         <td>
             {{ $user->username }}
-        </td><td>$user->country</td>
+        </td><td>{{$user->country}}</td>
     </tr>
 </table>
+
+<h1>Role and permissions</h1>
+<ul class="list-group-unbordered">
 @foreach($user->roles as $role)
-    <div>{{$role->name}}</div>
+    <li>{{$role->name}}</li>
 @endforeach
+</ul>
