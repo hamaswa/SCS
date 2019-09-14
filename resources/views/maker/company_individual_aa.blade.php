9794ecc4349cb79d6070e169bd9baf535c4cb902
Abdul Munem <munem.khetran@gmail.com>
2019-09-14 19:46:14 +0500

    company application kyc view update and new file created

---
+++ resources/views/maker/company_individual_aa.blade.php
@@ -0,0 +1,31 @@
+<div id="btn-air" class="btn-group margin-bottom border-black-1">
+    <button type="button" data-la="34" data-id="34" class="btn btn-default btn-flat la_aa">Mr ABC</button>
+    <button type="button" data-toggle="dropdown" aria-expanded="false" class="btn btn-default btn-flat dropdown-toggle">
+        <i class="fa fa-list"></i> <span class="sr-only">Toggle Dropdown</span></button>
+</div>
+<div id="btn-air" class="btn-group margin-bottom border-black-1">
+    <button type="button" data-la="34" data-id="34" class="btn btn-default btn-flat la_aa">Mr XYZ</button>
+    <button type="button" data-toggle="dropdown" aria-expanded="false" class="btn btn-default btn-flat dropdown-toggle">
+        <i class="fa fa-list"></i> <span class="sr-only">Toggle Dropdown</span></button>
+</div>
+<div class="clearfix"></div>
+<div class="col-sm-12 col-md-12 form-group">
+    <label>Full Name as Per NRIC / Passport</label>
+    <input type="text" value="" name="name" class="form-control" autocomplete="off">
+</div>
+<div class="col-sm-12 col-md-12 form-group">
+    <label>NRIC No. / Passport No.</label>
+    <input type="text" value="" name="unique_id" class="form-control" autocomplete="off">
+</div>
+<div class="col-sm-12 col-md-12 form-group">
+    <label>Mobile No.</label>
+    <input type="text" value="" name="mobile" class="form-control" autocomplete="off">
+</div>
+<div class="col-sm-12 col-md-12 form-group">
+    <label>Email</label>
+    <input type="email" name="email" value="" class="form-control" autocomplete="off">
+</div>
+<div class="col-sm-12 col-md-12 form-group">
+    <label>Residential Address</label>
+    <textarea name="address" class="form-control"></textarea>
+</div>
