3c86ffa679e7d72775afc51a46cee01a97a033bf
Hamayun Khan <hkhan.swa@gmail.com>
2019-10-05 19:40:38 +0500

    Guide lines to text area. Show all attached applicants in Pipeline search

---
+++ database/migrations/2019_10_05_143112_change_guide_lines_field_type.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeGuideLinesFieldType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('applicant_documents', function (Blueprint $table) {
+            $table->text("guide_lines")->change();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('applicant_documents', function (Blueprint $table) {
+            //
+        });
+    }
+}
