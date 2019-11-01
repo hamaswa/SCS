2ceb1eb0ded7d7cc14f5dfd6e6ab8853c139a629
Hamayun Khan <hkhan.swa@gmail.com>
2019-11-01 11:29:26 +0500

    Muliti User Over view

--- resources/views/aadata/comments.blade.php
+++ resources/views/aadata/comments.blade.php
@@ -1,45 +1,52 @@
-<div class="table-responsive">
-    <table class="table table-bordered table-striped table-hover bg-white">
-        <thead>
-        <tr class="bg-light-blue-gradient">
-            <th colspan="4" class="text-center">All Comments</th>
-        </tr>
-        <tr class="bg-aqua">
-            <th>Date</th>
-            <th>Status</th>
-            <th>By</th>
-        </tr>
+@if(isset($applicants))
+    @foreach($applicants as $applicant)
+        {{
+        $applicant->name
+        }}
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped table-hover bg-white">
+                <thead>
+                <tr class="bg-light-blue-gradient">
+                    <th colspan="4" class="text-center">All Comments</th>
+                </tr>
+                <tr class="bg-aqua">
+                    <th>Date</th>
+                    <th>Status</th>
+                    <th>By</th>
+                </tr>
 
-        </thead>
-        <tbody>
-        @foreach($comments as $comment)
-            <tr>
-                <td>{{ date("Y-m-d",strtotime($comment->created_at))}}</td>
-                <td></td>
-                <td>{{$comment->user->first_name}}</td>
-            </tr>
-            <tr>
-                <td colspan="3"><b>Comments:</b><br>{!! $comment->comments !!}</td>
-            </tr>
-        @endforeach
+                </thead>
+                <tbody>
+                @foreach($applicant->applicantComments as $comment)
+                    <tr>
+                        <td>{{ date("Y-m-d",strtotime($comment->created_at))}}</td>
+                        <td></td>
+                        <td>{{$comment->user->first_name}}</td>
+                    </tr>
+                    <tr>
+                        <td colspan="3"><b>Comments:</b><br>{!! $comment->comments !!}</td>
+                    </tr>
+                @endforeach
 
-        {{--<tr class="bg-red-gradient">--}}
-            {{--<td></td>--}}
-            {{--<td>KIV</td>--}}
-            {{--<td></td>--}}
-            {{--<td></td>--}}
-        {{--</tr>--}}
-        {{--<tr class="bg-green">--}}
-            {{--<td></td>--}}
-            {{--<td>KIV<br><small>replied</small></td>--}}
-            {{--<td></td>--}}
-            {{--<td></td>--}}
-        {{--</tr>--}}
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-        </tbody>
-    </table>
-</div>
+                {{--<tr class="bg-red-gradient">--}}
+                {{--<td></td>--}}
+                {{--<td>KIV</td>--}}
+                {{--<td></td>--}}
+                {{--<td></td>--}}
+                {{--</tr>--}}
+                {{--<tr class="bg-green">--}}
+                {{--<td></td>--}}
+                {{--<td>KIV<br><small>replied</small></td>--}}
+                {{--<td></td>--}}
+                {{--<td></td>--}}
+                {{--</tr>--}}
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    @endforeach
+@endif
