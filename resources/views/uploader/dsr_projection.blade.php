872830a9f58d88d00a1c36fff0d04d030f396483
Abdul Munem <munem.khetran@gmail.com>
2019-12-10 10:10:01 +0500

    dsr project view update

--- resources/views/uploader/dsr_projection.blade.php
+++ resources/views/uploader/dsr_projection.blade.php
@@ -22,28 +22,28 @@
 <div class="col-lg-12">
 
     <div class="col-md-3 bg-info padding-5">
-
+        <label class="bg-gray-light padding-5 text-center" style="width:100%">Income</label>
         <div class="dsr_projection_income_total" style="min-height:200px">
 
         </div>
         <input type="text" name="dsr_income_total" class="form-control" id="dsr_income_total" placeholder="Total">
     </div>
     <div class="col-md-3 bg-gray  padding-5">
-
+        <label class="bg-gray-light padding-5 text-center" style="width:100%">Exisiting Facility</label>
         <div class="dsr_projection_existing_facility_total" style="min-height:200px">
 
         </div>
         <input type="text" name="dsr_existing_facility_total" class="form-control" id="dsr_existing_facility_total">
     </div>
     <div class="col-md-3 bg-success padding-5">
-
+        <label class="bg-gray-light padding-5 text-center" style="width:100%">New Facility</label>
         <div class="dsr_projection_new_facility_total" style="min-height:200px">
 
         </div>
         <input type="text" name="dsr_new_facility_total" class="form-control" id="dsr_new_facility_total">
     </div>
     <div class="col-md-3 bg-danger padding-5">
-
+        <label class="bg-gray-light padding-5 text-center" style="width:100%">Exclusion</label>
         <div class="dsr_exclusion" style="min-height:235px">
 
         </div>
